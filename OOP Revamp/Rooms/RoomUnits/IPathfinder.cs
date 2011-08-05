3c2f29fd845713961562906ca2e61588acd0f24b
Chris Chenery <Cecer1@cecer1.com>
2011-08-05 10:04:03 +0100

    Added IHI.Server.Rooms.RoomUnits.IPathfinder. Added IHI.Server.Rooms.RoomUnit.GetSpeed(). Added IHI.Server.Rooms.RoomUnit.SetSpeed(float Speed).

---
+++ OOP Revamp/Rooms/RoomUnits/IPathfinder.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace IHI.Server.Rooms.RoomUnits
+{
+    public interface IPathfinder
+    {
+        void ApplyCollisionMap(byte[,] Map, float[,] Height);
+        ICollection<byte[]> Path(byte StartX, byte StartY, byte EndX, byte EndY, float MaxDrop, float MaxJump);
+    }
+}
